a287cacc575b7cbba23fa509616b538dc64603ee
anjarupnik <anjarupnik@hotmail.com>
2017-11-13 21:45:30 +0100

    user authentication

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
-const bodyParser = require('body-parser')
 const express = require('express')
-const { Class } = require('./models')
-const { users } = require('./routes')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const passport = require('./config/auth')
+const { users, sessions } = require('./routes')
 
-const PORT = process.env.PORT || 3030
+const port = process.env.PORT || 3030
 
 let app = express()
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.use((err, req, res, next) => {
-  res.status(err.status || 500)
-  res.send({
-    message: err.message,
-    // only print full errors in development
-    error: app.get('env') === 'development' ? err : {}
-  })
-})
+app
+  .use(cors())
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(bodyParser.json())
+  .use(passport.initialize())
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`)
-})
+  .use(users)
+  .use(sessions)
+
+  .use((req, res, next) => {
+    const err = new Error('Not Found')
+    err.status = 404
+    next(err)
+  })
+
+  .use((err, req, res, next) => {
+    res.status(err.status || 500)
+    res.send({
+      message: err.message,
+      error: app.get('env') === 'development' ? err : {}
+    })
+  })
+
+  .listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+  })
