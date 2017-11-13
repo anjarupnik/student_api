47d737c899432ebe7045b89ed42aba477c3be209
anjarupnik <anjarupnik@hotmail.com>
2017-11-13 21:01:38 +0100

    user schema

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
+const bodyParser = require('body-parser')
 const express = require('express')
 const { Class } = require('./models')
+const { users } = require('./routes')
 
 const PORT = process.env.PORT || 3030
 
 let app = express()
 
-app.get('/classes', (req, res, next) => {
-  Class.find()
-    .sort({ createdAt: -1 })
-    .then((classes) => res.json(classes))
-    // Forward any errors to error handler
-    .catch((error) => next(error))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.send({
+    message: err.message,
+    // only print full errors in development
+    error: app.get('env') === 'development' ? err : {}
+  })
 })
 
 app.listen(PORT, () => {
