5fc540b3a56f876b39e7768e2834ea20c6d662e6
anjarupnik <anjarupnik@hotmail.com>
2017-11-13 22:55:02 +0100

    batch and students routes

---
+++ models/batch.js
@@ -0,0 +1,13 @@
+const mongoose = require('../config/database')
+const { Schema } = mongoose
+
+const batchSchema = new Schema({
+  batchNumber: { type: Number, required: true},
+  startDate: { type: Date },
+  endDate: { type: Date },
+  students: [{}],
+  questions: [String],
+  questionsDate: { type: Date, default: Date.now},
+});
+
+module.exports = mongoose.model('batches', batchSchema)
--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
-const Class = require('./class')
+const Batch = require('./batch')
 const User = require('./user')
+const Student = require('./student')
 
 module.exports = {
-  Class,
-  User
+  Batch,
+  User,
+  Student
 }
